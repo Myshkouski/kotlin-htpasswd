9d4a763610894cad0a58297da0fb25841e727b99
Alexei <alexeimyshkouski@gmail.com>
2024-01-24 14:54:34 +0300

    remove deprecated api usage

--- htpasswd-micronaut/src/main/kotlin/dev/myshkouski/htpasswd/micronaut/HtpasswdAuthenticationProvider.kt
+++ htpasswd-micronaut/src/main/kotlin/dev/myshkouski/htpasswd/micronaut/HtpasswdAuthenticationProvider.kt
@@ -3,40 +3,33 @@ package dev.myshkouski.htpasswd.micronaut
 import dev.myshkouski.htpasswd.HtpasswdAuthenticator
 import dev.myshkouski.htpasswd.HtpasswdParser
 import io.micronaut.core.io.Readable
-import io.micronaut.security.authentication.AuthenticationProvider
+import io.micronaut.security.authentication.AuthenticationFailureReason
 import io.micronaut.security.authentication.AuthenticationRequest
 import io.micronaut.security.authentication.AuthenticationResponse
+import io.micronaut.security.authentication.provider.AuthenticationProvider
 import org.reactivestreams.Publisher
 import reactor.core.publisher.Flux
 import reactor.core.publisher.FluxSink
 import java.io.InputStream
 import java.util.*
 
-class HtpasswdAuthenticationProvider<T>(
+class HtpasswdAuthenticationProvider<T, I, S>(
     private val authenticator: HtpasswdAuthenticator,
     private val response: AuthenticationResponseBuilder = defaultResponse,
-) : AuthenticationProvider<T> {
+) : AuthenticationProvider<T, I, S> {
     constructor(htpasswdParser: HtpasswdParser, htpasswdLines: Array<String>, response: AuthenticationResponseBuilder = defaultResponse): this(HtpasswdAuthenticator(htpasswdParser, htpasswdLines), response)
     constructor(htpasswdParser: HtpasswdParser, input: InputStream, response: AuthenticationResponseBuilder = defaultResponse): this(htpasswdParser, input.readHtpasswdLines(), response)
     constructor(htpasswdParser: HtpasswdParser, readable: Readable, response: AuthenticationResponseBuilder = defaultResponse): this(htpasswdParser, readable.asInputStream(), response)
 
-    private fun AuthenticationRequest<*, *>.emitter() = { emitter: FluxSink<AuthenticationResponse> ->
-        val username = identity as String
-        val verified = authenticator.authenticate(username, secret as String?)
+    override fun authenticate(requestContext: T, authRequest: AuthenticationRequest<I, S>): AuthenticationResponse {
+        val username = authRequest.identity as String
+        val verified = authenticator.authenticate(username, authRequest.secret as String?)
 
-        if (verified) {
-            emitter.next(response(username))
-            emitter.complete()
-        } else {
-            emitter.error(AuthenticationResponse.exception())
+        if (!verified) {
+            throw AuthenticationResponse.exception(AuthenticationFailureReason.UNKNOWN)
         }
-    }
 
-    override fun authenticate(
-        httpRequest: T?,
-        authenticationRequest: AuthenticationRequest<*, *>?
-    ): Publisher<AuthenticationResponse> {
-        return Flux.create(authenticationRequest!!.emitter(), FluxSink.OverflowStrategy.ERROR)
+        return response(username)
     }
 }
 
